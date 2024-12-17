b945822bc3194dddc36572093b44e9991e746d03
brenprie <brenprie@gmail.com>
2024-12-17 10:25:21 -0500

    Updated table schema with character lengths

--- EmployeeSQL/Mod 9 Table Schema.sql
+++ EmployeeSQL/Mod 9 Table Schema.sql
@@ -2,46 +2,58 @@
 -- Link to schema: https://app.quickdatabasediagrams.com/#/d/EdpDsg
 -- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
--- CREATE TABLES, SPECIFYING PRIMARY KEYS; ADD FOREIGN KEYS
+
 CREATE TABLE "titles" (
-    "title_id" VARCHAR   NOT NULL,
-    "title" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_titles" PRIMARY KEY ("title_id")
+    "title_id" VARCHAR(5)   NOT NULL,
+    "title" VARCHAR(18)   NOT NULL,
+    CONSTRAINT "pk_titles" PRIMARY KEY (
+        "title_id"
+     )
 );
 
 CREATE TABLE "employees" (
     "emp_no" INT   NOT NULL,
-    "emp_title_id" VARCHAR   NOT NULL,
+    "emp_title_id" VARCHAR(5)   NOT NULL,
     "birth_date" DATE   NOT NULL,
-    "first_name" VARCHAR   NOT NULL,
-    "last_name" VARCHAR   NOT NULL,
-    "sex" VARCHAR   NOT NULL,
+    "first_name" VARCHAR(15)   NOT NULL,
+    "last_name" VARCHAR(15)   NOT NULL,
+    "sex" VARCHAR(1)   NOT NULL,
     "hire_date" DATE   NOT NULL,
-    CONSTRAINT "pk_employees" PRIMARY KEY ("emp_no")
+    CONSTRAINT "pk_employees" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 CREATE TABLE "departments" (
-    "dept_no" VARCHAR   NOT NULL,
-    "dept_name" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_departments" PRIMARY KEY ("dept_no")
+    "dept_no" VARCHAR(4)   NOT NULL,
+    "dept_name" VARCHAR(20)   NOT NULL,
+    CONSTRAINT "pk_departments" PRIMARY KEY (
+        "dept_no"
+     )
 );
 
 CREATE TABLE "dept_manager" (
-    "dept_no" VARCHAR   NOT NULL,
+    "dept_no" VARCHAR(4)   NOT NULL,
     "emp_no" INT   NOT NULL,
-    CONSTRAINT "pk_dept_manager" PRIMARY KEY ("dept_no","emp_no")
+    CONSTRAINT "pk_dept_manager" PRIMARY KEY (
+        "dept_no","emp_no"
+     )
 );
 
 CREATE TABLE "dept_emp" (
     "emp_no" INT   NOT NULL,
-    "dept_no" VARCHAR   NOT NULL,
-    CONSTRAINT "pk_dept_emp" PRIMARY KEY ("emp_no","dept_no")
+    "dept_no" VARCHAR(4)   NOT NULL,
+    CONSTRAINT "pk_dept_emp" PRIMARY KEY (
+        "emp_no","dept_no"
+     )
 );
 
 CREATE TABLE "salaries" (
     "emp_no" INT   NOT NULL,
     "salary" INT   NOT NULL,
-    CONSTRAINT "pk_salaries" PRIMARY KEY ("emp_no")
+    CONSTRAINT "pk_salaries" PRIMARY KEY (
+        "emp_no"
+     )
 );
 
 ALTER TABLE "employees" ADD CONSTRAINT "fk_employees_emp_title_id" FOREIGN KEY("emp_title_id")
@@ -61,13 +73,4 @@ REFERENCES "departments" ("dept_no");
 
 ALTER TABLE "salaries" ADD CONSTRAINT "fk_salaries_emp_no" FOREIGN KEY("emp_no")
 REFERENCES "employees" ("emp_no");
-
--- AFTER IMPORT CSV DATA FILES, VERIFY THAT THEY HAVE LOADED CORRECTLY
-SELECT * FROM titles;
-SELECT * FROM employees;
-SELECT * FROM departments;
-SELECT * FROM dept_manager;
-SELECT * FROM dept_emp;
-SELECT * FROM salaries;
-
 
